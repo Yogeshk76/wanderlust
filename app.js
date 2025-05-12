5641a83e4846dc13250b18221d143fef3c389c75
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-05-12 16:20:21 +0530

    init

--- app.js
+++ app.js
@@ -1,40 +1,94 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Listing = require('./models/listing.js');
-const path = require('path');
+const express=require("express");
+const app=express();
+const path=require("path");
+const mongoose=require("mongoose");
+const methodoverride=require("method-override")
+const ejsmate = require("ejs-mate")
+const ExpressError=require("./utils/expresserror.js")
+const listing=require("./routes/listing.js")
+const reviews=require("./routes/review.js")
+const user=require("./routes/user.js")
+const session = require("express-session")
+const flash= require("connect-flash")
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user");
+require('dotenv').config()
 
-const app = express();
 
-const MONGO_URL = 'mongodb://localhost:27017/wanderlust';
 
-main().then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.log('Error connecting to MongoDB:', err);
-})
 
-async function main() {
-  await mongoose.connect(MONGO_URL)
-}
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.get('/', (req, res) => {
-  res.send("Hi, I am root")
+async function main(){
+    await mongoose.connect(process.env.DB_HOST)
+};
+main().then(()=>{
+    console.log("connected to database")
+}).catch((err)=>{
+    console.log(err)
 });
 
-app.get('/listings', async (req, res) => {
-    try {
-      const allListings = await Listing.find();
-      res.render("listings/index.ejs", {allListings})
-    } catch (error) {
-        res.status(500).json({ message: error.message })
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodoverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("ejs",ejsmate);
+
+const sessionOptions={
+    secret:"mysupersecretcode",
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires: Date.now() + 7*24*60*60*1000,
+        maxAge: 7*24*60*60*1000,
+        httpOnly:true,
     }
+};
+
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req,res,next)=>{
+    res.locals.success=req.flash("success");
+    res.locals.error=req.flash("error");
+    res.locals.currUser=req.user;
+    next();
+}
+);
+
+
+app.use("/listing",listing)
+app.use("/listing/:id/reviews",reviews)
+app.use("/",user)
+app.get('/',(req,res)=>{
+    res.redirect('/listing')
 })
 
 
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
-}
-);
+
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page not found!"))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = 'Something went wrong!' } = err;
+    // Extract the last line of the error message
+    const lastLine = message.split('\n').pop();
+    console.log(message)
+    res.status(statusCode);
+    res.render('error.ejs', { err: { message: lastLine, statusCode } });
+});
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("server is started")
+})
--- init/index.js
+++ init/index.js
@@ -1,24 +1,19 @@
-const mongoose = require('mongoose');
-const initdata = require('./data.js');
-const Listing = require('../models/listing.js');
+const mongoose=require('mongoose')
+const initData=require("./newdata.js")
+const Listing= require("../models/listing.js")
 
-const MONGO_URL = 'mongodb://localhost:27017/wanderlust';
-
-main().then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.log('Error connecting to MongoDB:', err);
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust")
+};
+main().then(()=>{
+    console.log("connected to database")
+}).catch((err)=>{
+    console.log(err)
 })
 
-async function main() {
-  await mongoose.connect(MONGO_URL)
+
+const initDB=async ()=>{
+    await Listing.insertMany(initData.data)
 }
-
-const initDB = async () => {
-  await Listing.deleteMany({});
-
-  await Listing.insertMany(initdata.data);
-  console.log('data was initialized');
-};
 
 initDB();
