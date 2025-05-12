e0cf3c519318aefc4997d1a8c406e9e076412f7e
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-05-12 16:21:38 +0530

    package(json)

--- models/listing.js
+++ models/listing.js
@@ -1,21 +1,32 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose= require("mongoose");
+const schema=mongoose.Schema;
+const Review =require("./review.js")
 
-const listingSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: String,
-  image: {
-    filename: String,
-    url: String
-  },
-  price: Number,
-  location: String,
-  country: String,
-  });
+const listingSchema= new schema({
+    title:String,
+    description:String,
+    image:String,
+    price:Number,
+    location:String,
+    country:String,
+    features:String,
+    reviews:[
+        {
+            type: schema.Types.ObjectId,
+            ref:"Review"
+        }
+    ],
+    owner:{
+        type:schema.Types.ObjectId,
+        ref:"User"
+    }
+})
 
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+    await Review.deleteMany({_id: {$in:listing.reviews}})
+    }
+})
 
-  const Listing = mongoose.model('Listing', listingSchema);
-  module.exports = Listing;
+const Listing=mongoose.model("Listing",listingSchema)
+module.exports =Listing;
